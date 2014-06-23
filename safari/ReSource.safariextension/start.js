75efa003a2e484fd644151a83987cb7de3821c80
Aleksandr Sandrovskiy <a.sandrovsky@gmail.com>
2014-06-24 00:59:36 +0300

    Move logic to global page

--- safari/ReSource.safariextension/start.js
+++ safari/ReSource.safariextension/start.js
@@ -1,31 +1,20 @@
-document.addEventListener('beforeload', onBeforeLoad, true);
-
-var config = [
-    {
-        source: 'source.js',
-        reSource: 'reSource.js'
-    },
-    {
-        source: 'source.css',
-        reSource: 'reSource.css'
-    }
-];
-
 function onBeforeLoad(event) {
-    for (var i = 0; i < config.length; i++) {
-        var regexp = new RegExp(config[i].source);
-        if (regexp.test(event.url)) {
-            event.preventDefault();
-            if (event.srcElement.tagName == 'SCRIPT') {
-                var newElement = document.createElement('script');
-                newElement.setAttribute('src', config[i].reSource);
-                event.srcElement.parentNode.replaceChild(newElement, event.srcElement);
-            } else if (event.srcElement.tagName == 'LINK') {
-                var newElement = document.createElement('link');
-                newElement.setAttribute('rel', 'stylesheet');
-                newElement.setAttribute('href', config[i].reSource);
-                event.srcElement.parentNode.replaceChild(newElement, event.srcElement);
-            }
+    var source = event.url;
+    var reSource = safari.self.tab.canLoad(event, source);
+
+    if (reSource != source) {
+        event.preventDefault();
+        var oldNode = event.srcElement;
+        var newNode = document.createElement(oldNode.tagName);
+        if (oldNode.tagName == 'LINK') {
+            newNode.setAttribute('rel', 'stylesheet');
+            newNode.setAttribute('href', reSource);
+        } else {
+            newNode.setAttribute('type', 'text/javascript');
+            newNode.setAttribute('src', reSource);
         }
+        oldNode.parentNode.replaceChild(newNode, oldNode);
     }
 }
+
+document.addEventListener('beforeload', onBeforeLoad, true);
